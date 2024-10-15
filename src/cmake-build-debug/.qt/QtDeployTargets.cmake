ef1feabeba5f194b74f7e19b48dffef31baf657c
maraielciu1 <mara.ielciu@stud.ubbcluj.ro>
2024-10-15 22:41:00 +0300

    updated project

---
+++ src/cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_oop-a10-maraielciu1_FILE /Users/maraielciu/Desktop/project-oop/oop-a10-maraielciu1/cmake-build-debug/oop-a10-maraielciu1)
+set(__QT_DEPLOY_TARGET_oop-a10-maraielciu1_TYPE EXECUTABLE)
